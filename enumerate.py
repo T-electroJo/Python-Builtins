87ca5ac50491d384921f7815f97a9bece294acd8
electroJo <96352550+T-electroJo@users.noreply.github.com>
2022-06-25 12:32:41 +0200

    Initial commit

--- enumerate.py
+++ enumerate.py
@@ -1,5 +1,8 @@
+from typing import Any, Iterable
+
+
 def enumerate(iterable: Iterable) -> list[tuple[int, Any]]:
-    _out: list[tuple[int, Any]] = []
+    _out = []
     for _i in range(len(iterable)):
         _out.append(tuple([_i, iterable[_i]]))
     return _out
